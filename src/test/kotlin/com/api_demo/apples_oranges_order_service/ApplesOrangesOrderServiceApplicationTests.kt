cf31e4321812266e91d1a72966f3bbe069efce8e
Caleb Hutchison <calebhutch98@gmail.com>
2024-08-10 21:10:50 -0700

    feat: Implemented Step 1.

---
+++ src/test/kotlin/com/api_demo/apples_oranges_order_service/ApplesOrangesOrderServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.api_demo.apples_oranges_order_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplesOrangesOrderServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
